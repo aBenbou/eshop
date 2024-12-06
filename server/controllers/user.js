3793351bd6d76c23b91098251b3f1fe1d3189378
Aymane <aymane.benbouzid@outlook.com>
2024-12-06 15:51:38 +0100

    Added User related functions

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,38 +1,42 @@
 import { asyncError } from "../middlewares/error.js";
 import { User } from "../models/user.js";
 import ErrorHandler from "../utils/error.js";
+import { cookieOptions, sendToken } from "../utils/features.js";
 
 export const login = asyncError(async (req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email }).select("+password");
 
     if (!user) {
-        return res.status(400).json({
-            success: false,
-            message: "Incorrect Email"
-        });
+        return next(new ErrorHandler("Incorrect Email or Password", 400));
     }
+
+    if (!password)
+        return next(
+            new ErrorHandler("Please Enter Password", 400)
+        );
+
 
     //handle error
     const isMatched = await user.comparePassword(password);
     if (!isMatched) {
-        return next(new ErrorHandler("Incorrect Password", 400));
+        return next(new ErrorHandler("Incorrect Email or Password", 400));
     }
 
     //token
-
-    res.status(200).json({
-        success: true,
-        message: `Welcome Back ${user.name}`,
-    });
+    sendToken(user, res, `Welcome back, ${user.name}`, 200);
 });
 
 export const signup = asyncError(async (req, res, next) => {
     const { name, email, password, address, city, country, pinCode } = req.body;
 
+    let user = await User.findOne({ email });
+
+    if (user) return next(new ErrorHandler("User Already Exist", 400));
+
     // add cloudinary in future
 
-    await User.create({
+    user = await User.create({
         name,
         email,
         password,
@@ -42,8 +46,72 @@ export const signup = asyncError(async (req, res, next) => {
         pinCode,
     });
 
-    res.status(201).json({
+    //token
+    sendToken(user, res, `Registered ${user.name} Successfully`, 201);
+});
+
+export const logOut = asyncError(async (req, res, next) => {
+    res
+        .status(200)
+        .cookie("token", "", {
+            ...cookieOptions,
+            expires: new Date(Date.now()),
+        })
+        .json({
+            success: true,
+            message: "Logged Out Successfully",
+        });
+});
+
+
+export const getMyProfile = asyncError(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+
+    res.status(200).json({
         success: true,
-        message: "Registered s\Successfully"
+        user,
+    });
+});
+
+export const updateProfile = asyncError(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+
+    const { name, email, address, city, country, pinCode } = req.body;
+
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (address) user.address = address;
+    if (city) user.city = city;
+    if (country) user.country = country;
+    if (pinCode) user.pinCode = pinCode;
+
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Profile Updated Successfully",
+    });
+});
+
+export const changePassword = asyncError(async (req, res, next) => {
+    const user = await User.findById(req.user._id).select("+password");
+
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword)
+        return next(
+            new ErrorHandler("Please Enter Old Password & New Password", 400)
+        );
+
+    const isMatched = await user.comparePassword(oldPassword);
+
+    if (!isMatched) return next(new ErrorHandler("Incorrect Old Password", 400));
+
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Password Changed Successully",
     });
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { login, signup } from "../controllers/user.js";
+import { changePassword, getMyProfile, login, logOut, signup, updateProfile } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/new", signup);
+router.get("/me", isAuthenticated, getMyProfile);
+router.get("/logout", isAuthenticated, logOut);
+
+
+// Updating Routes
+router.put("/updateprofile", isAuthenticated, updateProfile);
+router.put("/changepassword", isAuthenticated, changePassword);
+// router.put("/updatepic", isAuthenticated, singleUpload, updatePic);
+
+// Forget Password & Reset Password
+// router.route("/forgetpassword").post(forgetpassword).put(resetpassword);
 
 
 export default router;
