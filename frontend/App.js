cf62393d2887d0df93aa59db71a016fcc8fb49a0
Aymane <aymane.benbouzid@outlook.com>
2024-12-08 13:26:19 +0100

    done ONLINE payment test stripe

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,24 @@
 import Main from "./Main";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { StripeProvider } from "@stripe/stripe-react-native";
+
+const stripeKey =
+  "pk_test_51QTNnhDAeoRetvrkKoZM8YUl39VDck78nN9NplIeorqqUceITCnCxu3mJw2CGyX1zIgAJATInkExKcAS8DVJV7vm00zmUKMf6u";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Main />
-    </Provider>
+    <StripeProvider
+      threeDSecureParams={{
+        backgroundColor: "#fff",
+        timeout: 5,
+      }}
+      merchantIdentifier="eshop-aymane"
+      publishableKey={stripeKey}
+    >
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    </StripeProvider>
   );
 }
--- frontend/screens/Payment.jsx
+++ frontend/screens/Payment.jsx
@@ -7,14 +7,20 @@ import { Button, RadioButton } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { placeOrder } from "../redux/actions/otherAction";
 import { useMessageAndErrorOther } from "../utils/hooks";
+import { useStripe } from "@stripe/stripe-react-native";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+import axios from "axios";
+import { server } from "../redux/store";
+import Loader from "../components/Loader";
 
 const Payment = ({ navigation, route }) => {
 
     const [ paymentMethod, setPaymentMethod ]  = useState('COD');
+    const [loaderLoading, setLoaderLoading] = useState(false);
 
 
     const dispatch = useDispatch();
-
+    const stripe = useStripe();
 
     const { isAuthenticated, user } = useSelector((state) => state.user);
     const { cartItems } = useSelector((state) => state.cart);
@@ -49,9 +55,55 @@ const Payment = ({ navigation, route }) => {
           );
 
     }
-    const onlineHandler = ()=> {
-        
-    }
+    const onlineHandler = async () => {
+        try {
+          const {
+            data: { client_secret },
+          } = await axios.post(
+            `${server}/order/payment`,
+            {
+              totalAmount: route.params.totalAmount,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+              withCredentials: true,
+            }
+          );
+    
+          const init = await stripe.initPaymentSheet({
+            paymentIntentClientSecret: client_secret,
+            merchantDisplayName: "eshop-aymane",
+          });
+    
+          if (init.error)
+            return Toast.show({ type: "error", text1: init.error.message });
+    
+          const presentSheet = await stripe.presentPaymentSheet();
+          setLoaderLoading(true);
+    
+          if (presentSheet.error) {
+            setLoaderLoading(false);
+            return Toast.show({ type: "error", text1: presentSheet.error.message });
+          }
+    
+          const { paymentIntent } = await stripe.retrievePaymentIntent(
+            client_secret
+          );
+    
+          if (paymentIntent.status === "Succeeded") {
+            codHandler({ id: paymentIntent.id, status: paymentIntent.status });
+          }
+        } catch (error) {
+          return Toast.show({
+            type: "error",
+            text1: "Some Error",
+            text2: error,
+          });
+        }
+      };
+    
 
     const loading = useMessageAndErrorOther(
         dispatch,
@@ -63,7 +115,8 @@ const Payment = ({ navigation, route }) => {
       );
 
     return (
-        <View style={defaultStyle}>
+        loaderLoading ? <Loader /> : (
+            <View style={defaultStyle}>
 
             <Header back={true} />
             <Heading
@@ -126,6 +179,7 @@ const Payment = ({ navigation, route }) => {
                 </Button>
             </TouchableOpacity>
         </View>
+        )
     );
 };
 
